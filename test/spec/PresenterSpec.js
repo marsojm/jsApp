a0ed17a13edc8e7e623415c35ba45ff6d7866fc8
Marko Malinen <markojm1986@gmail.com>
2015-07-31 18:31:13 +0300

    Add more tests

--- test/spec/PresenterSpec.js
+++ test/spec/PresenterSpec.js
@@ -1,7 +1,15 @@
+var Error = function(msg) {
+  this.msg = msg;
+};
+
 describe("Presenter", function(){
   var self = this;
   var presenter;
   var data;
+  var receivedAction;
+  self.handleAction = function(action) {
+    receivedAction = action;
+  };
 
   beforeEach(function() {
     presenter = new Presenter(self);
@@ -12,16 +20,42 @@ describe("Presenter", function(){
             errors:[],
             isSuccess: false,
     };
+    receivedAction = null;
   });
 
   describe("when data has been loaded", function() {
     it("should have right number of persons", function() {
       var expected = 4;
       presenter.load(data);
-      console.log(presenter);
       var vmo = presenter.getVMO();
       expect(vmo.items.length).toEqual(expected);
     });
   });
 
+  describe("when delete action is performed", function() {
+    beforeEach(function() {
+      presenter.load(data);
+    });
+
+    it("the REMOVE action should be called with right parameters", function() {
+      presenter.sendAction({action:'REMOVE',params: {id: 3}});
+      expect(receivedAction.action).toEqual('REMOVE');
+      expect(receivedAction.params.id).toEqual(3);
+    });
+  });
+
+  describe("when update action is performed", function() {
+    beforeEach(function() {
+      presenter.load(data);
+    });
+
+    it("the Update action should be called with right parameters", function() {
+      presenter.sendAction({action:'UPDATE',params: {data:{id:1,name:"Joe", age:55, salary:300}}});
+      expect(receivedAction.action).toEqual('UPDATE');
+      expect(receivedAction.params.data.id).toEqual(1);
+      expect(receivedAction.params.data.name).toEqual("Joe");
+      expect(receivedAction.params.data.age).toEqual(55);
+      expect(receivedAction.params.data.salary).toEqual(300);
+    });
+  });
 });
